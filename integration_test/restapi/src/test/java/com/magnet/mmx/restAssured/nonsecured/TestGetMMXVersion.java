48f63f1a2a8e89405d7bc1ff11b1e90611d4ad89
evance-magnet <evance.mok@magnet.com>
2015-08-05 09:40:04 -0700

    Fix version number in the integration test.

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.0.2-SNAPSHOT")).
+                        body("version", equalTo("1.3.0-SNAPSHOT")).
                         extract().asString();
     }
 }
