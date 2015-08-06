6daa33ee96a0df1d3847a075daaaf51b6bc12e1d
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-06 00:30:06 +0000

    Changing version from 1.5.1-SNAPSHOT to 1.8.1-SNAPSHOT

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.5.1-SNAPSHOT")).
+                        body("version", equalTo("1.8.1-SNAPSHOT")).
                         extract().asString();
     }
 }
