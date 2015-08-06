444653811de1e8ab77dd2a49a89b535fd954b156
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-06 23:16:39 +0000

    Changing version from 1.3.11 to 1.3.12

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.3.11")).
+                        body("version", equalTo("1.3.12")).
                         extract().asString();
     }
 }
