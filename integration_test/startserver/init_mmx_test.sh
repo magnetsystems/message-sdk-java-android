54a31f25f681d17f81f245ccc6264782b0e0627c
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-13 07:22:55 +0000

    Changing version from 1.4.5 to 1.4.6

--- integration_test/startserver/init_mmx_test.sh
+++ integration_test/startserver/init_mmx_test.sh
@@ -17,7 +17,7 @@
 
 curdir=`pwd`
 
-MMX_SERVER_NAME="mmx-server-1.4.5"
+MMX_SERVER_NAME="mmx-server-1.4.6"
 
 seeddata_sql="$curdir/../test-conf/seed_testdata.sql"
 cleanup_sql="$curdir/../test-conf/clean_testdata.sql"
