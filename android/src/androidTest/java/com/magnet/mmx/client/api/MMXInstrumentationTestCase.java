41e9d2300994f766552349f57d77a29addf81d9e
evance-magnet <evance.mok@magnet.com>
2015-08-12 19:33:33 -0700

    Fix unit tests that were failing on the build server.
    This was caused because the order of the tests that are running.
    Made the tests independent of each other.

--- android/src/androidTest/java/com/magnet/mmx/client/api/MMXInstrumentationTestCase.java
+++ android/src/androidTest/java/com/magnet/mmx/client/api/MMXInstrumentationTestCase.java
@@ -10,6 +10,9 @@ import com.magnet.mmx.client.common.Log;
 abstract public class MMXInstrumentationTestCase extends InstrumentationTestCase {
   private static final String TAG = MMXInstrumentationTestCase.class.getSimpleName();
   private Context mContext;
+  protected static final String USERNAME_PREFIX = "mmxusertest";
+  protected static final byte[] PASSWORD = "test".getBytes();
+  protected static final String DISPLAY_NAME_PREFIX = "MMX TestUser";
 
   private final MagnetMessage.OnFinishedListener<Void> mSessionListener =
           new MagnetMessage.OnFinishedListener<Void>() {
@@ -48,5 +51,42 @@ abstract public class MMXInstrumentationTestCase extends InstrumentationTestCase
 
   protected Context getContext() {
     return mContext;
+  }
+
+  protected void registerUser(String username, String displayName, String email, byte[] password) {
+    MMXUser user = new MMXUser.Builder()
+            .email(email)
+            .displayName(displayName)
+            .username(username)
+            .build();
+
+    //setup the listener for the registration call
+    final MagnetMessage.OnFinishedListener<Boolean> listener =
+            new MagnetMessage.OnFinishedListener<Boolean>() {
+              public void onSuccess(Boolean result) {
+                Log.d(TAG, "onSuccess: result=" + result);
+                synchronized (this) {
+                  this.notify();
+                }
+              }
+
+              public void onFailure(MagnetMessage.FailureCode code, Exception ex) {
+                Log.e(TAG, "onFailure(): code=" + code, ex);
+                synchronized (this) {
+                  this.notify();
+                }
+              }
+            };
+
+    //Register the user.  This may fail
+    MMXUser.register(user, password, listener);
+    synchronized (listener) {
+      try {
+        listener.wait(10000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
   }
 }
