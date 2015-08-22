d3ab38b074410f2500c96b10f04c1d80d59476a8
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-22 16:48:48 +0000

    Changing version from 1.5.4 to 1.5.5

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.5.4")).
+                        body("version", equalTo("1.5.5")).
                         extract().asString();
     }
 }
--- integration_test/startserver/init_mmx_test.sh
+++ integration_test/startserver/init_mmx_test.sh
@@ -17,7 +17,7 @@
 
 curdir=`pwd`
 
-MMX_SERVER_NAME="mmx-server-1.5.4"
+MMX_SERVER_NAME="mmx-server-1.5.5"
 
 seeddata_sql="$curdir/../test-conf/seed_testdata.sql"
 cleanup_sql="$curdir/../test-conf/clean_testdata.sql"
