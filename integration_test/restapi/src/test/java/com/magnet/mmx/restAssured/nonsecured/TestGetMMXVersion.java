9d11e99ac4abccdf13b439b874cef293576d5017
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-24 23:21:09 +0000

    Changing version from 1.5.6 to 1.5.7

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.5.6")).
+                        body("version", equalTo("1.5.7")).
                         extract().asString();
     }
 }
