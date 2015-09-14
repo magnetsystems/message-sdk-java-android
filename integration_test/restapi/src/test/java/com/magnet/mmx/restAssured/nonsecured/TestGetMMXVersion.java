e1d55d962057a3cb1e4bfd6f17f0913e991a725e
Jenkins Autobot <releaseengineering@magnet.com>
2015-09-14 19:16:06 +0000

    Changing version from 1.8.1 to 1.8.2

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("1.8.1")).
+                        body("version", equalTo("1.8.2")).
                         extract().asString();
     }
 }
