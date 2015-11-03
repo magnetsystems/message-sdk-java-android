4a571b3b055c0cff10ef373971913b4f6ed411c8
Jenkins Autobot <releaseengineering@magnet.com>
2015-11-03 01:55:41 +0000

    Changing version from 2.0.2 to 2.0.3

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("2.0.2")).
+                        body("version", equalTo("2.0.3")).
                         extract().asString();
     }
 }
