46b17740cb133428571d5af040b7cd1e3c8bde6a
Jenkins Autobot <releaseengineering@magnet.com>
2015-11-03 05:49:29 +0000

    Changing version from 2.0.8 to 2.0.9

--- integration_test/startserver/init_mmx_test.sh
+++ integration_test/startserver/init_mmx_test.sh
@@ -17,7 +17,7 @@
 
 curdir=`pwd`
 
-MMX_SERVER_NAME="mmx-server-2.0.8"
+MMX_SERVER_NAME="mmx-server-2.0.9"
 
 seeddata_sql="$curdir/../test-conf/seed_testdata.sql"
 cleanup_sql="$curdir/../test-conf/clean_testdata.sql"
