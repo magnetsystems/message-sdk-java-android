6b1504bcd2ac8c293e1440f66f0e9ca495cd89aa
Jenkins Autobot <releaseengineering@magnet.com>
2016-01-17 01:10:31 +0000

    Changing version from 2.3.0-b-1 to 2.3.0-b-3

--- integration_test/startserver/init_mmx_test.sh
+++ integration_test/startserver/init_mmx_test.sh
@@ -17,7 +17,7 @@
 
 curdir=`pwd`
 
-MMX_SERVER_NAME="mmx-server-2.3.0-b-1"
+MMX_SERVER_NAME="mmx-server-2.3.0-b-3"
 
 seeddata_sql="$curdir/../test-conf/seed_testdata.sql"
 cleanup_sql="$curdir/../test-conf/clean_testdata.sql"
