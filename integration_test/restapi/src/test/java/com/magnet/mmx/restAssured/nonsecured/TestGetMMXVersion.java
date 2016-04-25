0cca9086111137639e54ae8ca0056e5b7510bb2f
Jenkins Autobot <releaseengineering@magnet.com>
2016-04-25 13:49:27 -0700

    Changing version from 2.7.0-SNAPSHOT to 2.6.0-B-BUILD_NUMBER

--- integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
+++ integration_test/restapi/src/test/java/com/magnet/mmx/restAssured/nonsecured/TestGetMMXVersion.java
@@ -41,7 +41,7 @@ public class TestGetMMXVersion extends TestCase{
                         get("mmx/version").
                 then().
                         statusCode(200).
-                        body("version", equalTo("2.7.0-SNAPSHOT")).
+                        body("version", equalTo("2.6.0-B-BUILD_NUMBER")).
                         extract().asString();
     }
 }
