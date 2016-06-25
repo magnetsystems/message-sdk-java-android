ee22d77c25733053a21b9f9413c24eed1d7e6ada
Vincent Lau <vincent.lau@magnet.com>
2016-06-25 15:52:07 -0700

    Document this package.

---
+++ j2se/src/main/java/com/magnet/max/common/package-info.java
@@ -0,0 +1,6 @@
+package com.magnet.max.common
+/**
+ * This package contains all the verbatim classes declared in MAX
+ * but commonly shared with Java client.  This package has no external
+ * dependency.
+ */
